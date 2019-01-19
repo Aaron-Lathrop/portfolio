7a15a89bd1e35f9714236b951f05d1f7708d7c0b
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2019-01-18 18:55:16 -0700

    remove unnecessary js code

--- js/index.js
+++ js/index.js
@@ -1,45 +1,5 @@
 'use strict';
 
-// function addSkills(images) {
-//     let output = ``;
-//     images.forEach(image => {
-//         output += `<div class="skills-container">
-//                 <img class="skills-image" src="img/logos/${image.url}" alt="${image.name}">
-//                 <p class=skills-name>${image.name}</p>
-//             </div>`
-//     });
-//     document.getElementById("skills-box").innerHTML = output;
-// };
-
-// function addProjectSkills(skillList) {
-//     let skills = "";
-//     skillList.forEach(skill => {
-//         skills += `<li class="skill-item">${skill}</li>`
-//     });
-//     return skills;
-// }
-
-// function addProjects(projects) {
-//     let output = `<h2>Projects</h2>`;
-//     projects.forEach(project => {
-//         output += `
-//         <div class="projects-container">
-//             <div class="projects-content">
-//                 <img class="projects-image" src="img/${project.img}" alt="${project.description}">
-//                 <p class="projects-description">
-//                     ${project.description}
-//                 </p>
-//                 <ul class="skill-list">${addProjectSkills(project.skills)}</ul>
-//                 <div class="project-btn-container">
-//                     <a href=${project.github} class="btn project-btn" target="_blank">Github</a>
-//                     <a href=${project.url} class="btn project-btn" target="_blank">Demo</a>
-//                 </div>
-//             </div>
-//         </div>  `;
-//     });
-//     document.getElementById("projects").innerHTML = output;
-// }
-
 function handleMobile() {
     document.getElementById('nav-list').classList.toggle('nav-show');
 }
@@ -49,10 +9,3 @@ function handleResize() {
         document.getElementById('nav-list').classList.remove('nav-show');
     }
 }
-
-// function onPageLoad() {
-//     addSkills(images);
-//     addProjects(projects);
-// }
-
-// onPageLoad();
